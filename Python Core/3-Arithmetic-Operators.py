9fde4cfc9caf8ea40d0eb773e2642cead0ba454f
tusshar17 <tr.jakhara019@gmail.com>
2023-03-26 21:29:19 +0530

    Arithmetic Operators

---
+++ Python Core/3-Arithmetic-Operators.py
@@ -0,0 +1,70 @@
+# ===== Arithmetic Operators =====
+
+
+# Addition (+)
+print(4+2)
+
+a = 4
+b = 2
+total = a + b
+print(total)
+
+
+# Subtraction (-)
+print(4-2)
+
+a = 4
+b = 2
+sub = a - b
+print(sub)
+
+
+# Multiplication (*)
+print(4*2)
+
+a = 4
+b = 2
+multi = a * b
+print(multi)
+
+
+# Division (/)
+print(4/2)
+
+a = 4
+b = 2
+div = a / b
+print(div)
+print(type(div))
+
+
+# Modulus (%)
+print(5 % 2)
+
+a = 5
+b = 2
+mod = a % b
+print(mod)
+
+
+# Exponent (**)
+print(5**2)
+
+a = 5
+b = 2
+exp = a ** b
+print(exp)
+
+
+# Floor Division (//)
+print(5//2)
+
+a = 5
+b = 2
+sub = a // b
+print(sub)
+
+a = -5
+b = 2
+sub = a // b
+print(sub)
