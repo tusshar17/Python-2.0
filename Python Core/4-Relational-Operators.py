87c9d199251469d677278240bcbb5d1152af55e6
tusshar17 <tr.jakhara019@gmail.com>
2023-03-27 22:40:07 +0530

    Realtional Operators

---
+++ Python Core/4-Relational-Operators.py
@@ -0,0 +1,50 @@
+# ===== Relational Operators =====
+
+
+# Less than (<)
+print(5 < 2)
+
+a = 5
+b = 2
+value = a < b
+print(value)
+
+# Greater than(>)
+print(5 > 2)
+
+a = 5
+b = 2
+value = a > b
+print(value)
+
+# Less than or Equal to (<=)
+print(5 <= 2)
+
+a = 5
+b = 2
+value = a <= b
+print(value)
+
+# Greater than or Equal to
+print(5 >= 2)
+
+a = 5
+b = 2
+value = a >= b
+print(value)
+
+# Equal to
+print(5 == 2)
+
+a = 5
+b = 2
+value = a == b
+print(value)
+
+# Not Equal to
+print(5 != 2)
+
+a = 5
+b = 2
+value = a != b
+print(value)
