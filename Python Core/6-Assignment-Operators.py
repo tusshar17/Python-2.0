b8264fc36b50e7e8f4f8a9b5f3afd805329bd889
tusshar17 <tr.jakhara019@gmail.com>
2023-03-28 06:06:52 +0530

    Assignment Operators

---
+++ Python Core/6-Assignment-Operators.py
@@ -0,0 +1,44 @@
+# ===== Assignment Operators =====
+
+
+a = 10
+b = 20
+
+c = a+b
+print(c)
+
+
+# +=
+m = 15
+m += 10
+print(m)
+
+# -=
+m = 15
+m -= 10
+print(m)
+
+# *=
+m = 15
+m *= 10
+print(m)
+
+# /=
+m = 15
+m /= 10
+print(m)
+
+# %=
+m = 15
+m %= 10
+print(m)
+
+# **=
+m = 15
+m **= 10
+print(m)
+
+# //=
+m = 15
+m //= 10
+print(m)
