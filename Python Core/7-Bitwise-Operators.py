35c95af57ab83e576344ac919b5bd9e85110d45e
tusshar17 <tr.jakhara019@gmail.com>
2023-03-29 13:34:49 +0530

    Bitwise Operators

---
+++ Python Core/7-Bitwise-Operators.py
@@ -0,0 +1,12 @@
+# ===== Bitwise Operators =====
+
+
+a = 10                          # 0000 1010
+b = 15                          # 0000 1111
+
+print('~a = ', ~a)
+print('a&b = ', a & b)
+print('a|b = ', a | b)
+print('a^b = ', a ^ b)
+print('a<<2 = ', a << 2)
+print('a>>2 = ', a >> 2)
