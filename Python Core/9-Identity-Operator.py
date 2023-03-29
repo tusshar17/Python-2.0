214883309727c9737a58e6de2f57129f45548c8b
tusshar17 <tr.jakhara019@gmail.com>
2023-03-29 17:08:59 +0530

    Identity Operators

---
+++ Python Core/9-Identity-Operator.py
@@ -0,0 +1,20 @@
+# ===== Identity Operators =====
+
+# is
+a = 10
+b = 10
+print(a is b)
+
+a = 10
+b = '10'
+print(a is b)
+
+
+# is not
+a = 10
+b = 10
+print(a is not b)
+
+a = 10
+b = '10'
+print(a is not b)
