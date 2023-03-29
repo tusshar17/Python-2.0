2184ec6e8213a6a542a8ca29d11daf7a85f0ae6c
tusshar17 <tr.jakhara019@gmail.com>
2023-03-29 16:52:32 +0530

    Membership Operators

---
+++ Python Core/8-Membership-Operators.py
@@ -0,0 +1,25 @@
+# ===== Membership Operators =====
+
+
+# in
+
+str1 = "Welocme to India"
+print("to" in str1)
+
+str2 = "Welocme top Indians"
+print("to" in str2)
+
+str1 = "Welocme to India"
+print("into" in str1)
+
+
+# not in
+
+str1 = "Welcome to India"
+print("into" not in str1)
+
+str2 = "Hello World"
+print("bye" not in str1)
+
+str1 = "Welcome top coder"
+print("to" not in str1)
