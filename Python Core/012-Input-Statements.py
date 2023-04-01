c0b4f34f83d2cdbfd87b2a0b12c1055b6e4c04c7
tusshar17 <tr.jakhara019@gmail.com>
2023-04-02 02:56:22 +0530

    Input Statements

---
+++ Python Core/012-Input-Statements.py
@@ -0,0 +1,15 @@
+# ===== Output Statements =====
+
+name = input("Enter Your Name: ")
+print(name)
+
+
+# converting user input type
+
+mobile = int(input("Enter Mobile Number: "))
+print(mobile)
+print(type(mobile))
+
+price = float(input("Enter Price: "))
+print(price)
+print(type(price))
