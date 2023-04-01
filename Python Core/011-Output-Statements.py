47aa58f1c9e5fe12cfde2272737aaac686ead71e
tusshar17 <tr.jakhara019@gmail.com>
2023-04-01 19:51:38 +0530

    Output Statements

---
+++ Python Core/011-Output-Statements.py
@@ -0,0 +1,51 @@
+# ===== Output Statements =====
+
+print()     # it displays an empty line
+
+
+print("Hello World")
+print()
+print('Hello World')
+print("Eat", "Code", "Vibe")
+print(10)
+print()
+print("Hello")
+print("World")
+
+
+# passing object in print function
+data = [10, 20, -50, 21.3, 'HelloWorld']
+print(data)
+
+# sep
+print("Eat", "Code", "Vibe", sep=' ')
+print("Eat", "Code", "Vibe", sep='***')
+
+# end
+print("Welcome", end=' ')
+print("to", end=' ')
+print("India")
+
+print("Hello", end='\t')
+print("World")
+
+# printing variables
+
+a = 10
+print(a)
+
+x = 10
+y = 20
+print(x, y)
+
+print(x, y, sep=', ')
+
+
+# printing variables along with strings
+
+m = 40
+print("Value: ", m)
+
+name = "Rahul"
+age = 28
+print("My name is", name, "and my age is", age)
