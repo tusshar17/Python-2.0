8a8523064caeba613b91ede75e8354f77520463c
tusshar17 <tr.jakhara019@gmail.com>
2023-04-04 04:07:36 +0530

    If Else statements

---
+++ Python Core/017-If-Else-Statements.py
@@ -0,0 +1,10 @@
+# ===== If Else Statements=====
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+if a > b:
+    print(a, "is greater than", b)
+    print(a, "is greater than", b)
+else:
+    print(b, "is greater than", a)
