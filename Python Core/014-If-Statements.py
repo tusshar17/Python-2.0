4518e69c1dd9ff1582df7937c7c60439db025b24
tusshar17 <tr.jakhara019@gmail.com>
2023-04-03 15:52:13 +0530

    If Statement completed

--- Python Core/014-If-Statements.py
+++ Python Core/014-If-Statements.py
@@ -1,1 +1,20 @@
 # ===== If Statements =====
+
+if True:
+    print("Condition is True")
+
+if 5 > 2:
+    print("Condition True")
+    print("5 is greater than 2")
+print("rest of the code")
+
+
+if 5 < 2:
+    print("inside the condition statement")
+print("rest of the code")
+
+
+a = int(input("Enter a number greater than 5: "))
+
+if a > 5:
+    print("You entered:", a)
