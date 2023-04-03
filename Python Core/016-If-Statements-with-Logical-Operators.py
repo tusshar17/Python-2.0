6b18dda8645f616e276fd5811d4f336acca608ff
tusshar17 <tr.jakhara019@gmail.com>
2023-04-04 03:51:30 +0530

    If statements with logical operators

---
+++ Python Core/016-If-Statements-with-Logical-Operators.py
@@ -0,0 +1,9 @@
+# ===== If Statements with Logical Operators =====
+
+if 5 > 2 and 7 > 3:
+    print("5 greater than 2 and 7 greater than 3")
+print("rest of the code")
+
+
+if 5 > 2 or 7 < 3:
+    print("either of condition is true")
