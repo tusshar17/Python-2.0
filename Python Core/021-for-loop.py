b38658864b8421b43cd102e0f6f15a701d63a4a4
tusshar17 <tr.jakhara019@gmail.com>
2023-04-06 22:33:56 +0530

    for loops completed

--- Python Core/021-for-loop.py
+++ Python Core/021-for-loop.py
@@ -6,3 +6,45 @@ for ch in st:
     print(ch)
 
 print("rest of the code")
+
+
+# for loop using range() function
+for i in range(5):
+    print(i)
+
+print("-"*5)
+
+for i in range(1, 5):
+    print(i)
+
+print("-"*5)
+
+for i in range(1, 10, 2):
+    print(i)
+
+print("-"*5)
+
+
+st = "HelloWorld"
+st_length = len(st)
+
+for i in range(st_length):
+    print(i, ":", st[i])
+
+print("-"*5)
+
+
+# for loop with else
+
+for i in range(0, 10, 2):
+    print(i)
+else:
+    print("else part")
+
+
+# nested for loop
+
+for i in range(1, 5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
