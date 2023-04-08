1ddba533362295d03f45aa825b8b417a687a4065
tusshar17 <tr.jakhara019@gmail.com>
2023-04-08 10:28:18 +0530

    Accessing Array elements using while loop

---
+++ Python Core/025-Accessing-Array-Elements-Using-While-Loop.py
@@ -0,0 +1,12 @@
+# Accessing Array elements using while loop
+
+from array import *
+
+stu_roll = array('i', [101, 102, 103, 104, 105])
+
+n = len(stu_roll)
+i = 0
+
+while i < n:
+    print(stu_roll[i])
+    i += 1
