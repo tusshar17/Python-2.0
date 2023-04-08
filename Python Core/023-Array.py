e6f998c6ac642118e0e48d1e76dd4264ff8b4bff
tusshar17 <tr.jakhara019@gmail.com>
2023-04-08 08:25:49 +0530

    Importing aray module, creating arrays, accessing array elements

--- Python Core/023-Array.py
+++ Python Core/023-Array.py
@@ -1,9 +1,36 @@
 # ===== Array =====
 
+
 # import array
-import array
 
-stu_roll = array.array('i', [101, 102, 103, 104, 105])
+# method 1
+'''
+import array as ar
 
-for element in stu_roll:
-    print(element)
+stu_roll = ar.array('i', [101, 102, 103, 104, 105])
+
+print(stu_roll[0])
+print(stu_roll[1])
+print(stu_roll[2])
+print(stu_roll[3])
+print(stu_roll[4])
+'''
+
+# method 2
+
+from array import *
+
+stu_roll = array('i', [101, 102, 103, 104, 105])
+
+print(stu_roll[0])
+print(stu_roll[1])
+print(stu_roll[2])
+print(stu_roll[3])
+print(stu_roll[4])
+
+
+price = array('f', [10.5, 20.2, 15])
+
+print(price[0])
+print(price[1])  # something different
+print(price[2])
