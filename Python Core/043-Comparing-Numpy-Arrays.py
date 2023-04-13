576c169e09d00d005aac347fd808fe4792c14e40
tusshar17 <tr.jakhara019@gmail.com>
2023-04-13 12:46:21 +0530

    Comparing Numpy Arrays using Relational Operators, any() and all() functions

--- Python Core/043-Comparing-Numpy-Arrays.py
+++ Python Core/043-Comparing-Numpy-Arrays.py
@@ -6,7 +6,14 @@ a = array([100, 200, 300, 400, 500])
 b = array([100, 20, 30, 400, 50])
 
 result = a == b
-print(result)
+print("result :", result)
 
-result = a < b
-print(result)
+result2 = a < b
+print("result2 :", result2)
+
+
+# any() function
+print("any(result) :", any(result))
+
+# all() function
+print("all(result) :", all(result))
