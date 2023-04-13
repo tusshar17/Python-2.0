4f61019bbd122ca81504536051a15e3dfcf25bcc
tusshar17 <tr.jakhara019@gmail.com>
2023-04-13 12:23:31 +0530

    Comparing Numpy Arrays using Relational Operators

---
+++ Python Core/043-Comparing-Numpy-Arrays.py
@@ -0,0 +1,12 @@
+# Comparing Numpy Arrays using Relational Operators
+
+from numpy import *
+
+a = array([100, 200, 300, 400, 500])
+b = array([100, 20, 30, 400, 50])
+
+result = a == b
+print(result)
+
+result = a < b
+print(result)
