4e003ba203eb77c2f7ea00f8d8e6ebaf3278ba76
tusshar17 <tr.jakhara019@gmail.com>
2023-04-15 17:24:21 +0530

    Numpy 2D array using zeros() and ones()

---
+++ Python Core/051-NP-2D-Array-using-zeros-ones.py
@@ -0,0 +1,13 @@
+# Numpy 2D array using zeros() and ones()
+
+import numpy as np
+
+# zeros()
+a = np.zeros((3, 2), dtype=int)
+print(a)
+
+print("-"*10)
+
+# ones()
+b = np.ones((3, 2), dtype=int)
+print(b)
