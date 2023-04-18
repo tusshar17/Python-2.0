1db9866e03112415fc14e3e9d95cc45854ba53d0
tusshar17 <tr.jakhara019@gmail.com>
2023-04-18 09:06:25 +0530

    Strings

---
+++ Python Core/056-Strings.py
@@ -0,0 +1,37 @@
+# Strings
+
+# single quotes
+str1 = 'Hello World'
+print(str1)
+
+# double quotes
+str2 = "Hello World"
+print(str2)
+
+# Triple Single Quotes
+str3 = '''Hello World
+          This is Python
+          programming language.'''
+print(str3)
+
+# Triple Double Quotes
+str4 = """Hello World
+          This is Python
+          programming language."""
+print(str4)
+
+# Double Quote inside Single Quotes
+str5 = 'Hello "World" How are you'
+print(str5)
+
+# Single Quote inside Double Quotes
+str6 = "Hello 'World' How are you"
+print(str6)
+
+# using escape characters
+str7 = "Hello\nHow are you?"
+print(str7)
+
+# Raw String
+str8 = r"Hello\nHow are you?"
+print(str8)
