10ffe56a95dc4eec60c71bb49d1e6eb0494c8a76
tusshar17 <tr.jakhara019@gmail.com>
2023-04-19 11:09:56 +0530

    Comparing Strings

---
+++ Python Core/061-Comparing-Strings.py
@@ -0,0 +1,16 @@
+# Comparing Strings
+
+s1 = "HelloWorld"
+s2 = "HelloWorld"
+res = s1 == s2
+print(res)
+
+s3 = "HelloWorld"
+s4 = "Python"
+res = s3 == s4
+print(res)
+
+s5 = "A"
+s6 = "B"
+res = s5 < s6  # works according to the english dictionary order
+print(res)
