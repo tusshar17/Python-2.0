6ddf586a92bcf0a9e788cdcca52d5387f86ea104
tusshar17 <tr.jakhara019@gmail.com>
2023-04-19 11:01:12 +0530

    Repetition and concatenation  Operator

---
+++ Python Core/060-Repetition-and-Concatenation-Operator.py
@@ -0,0 +1,16 @@
+# Repetition and concatenation  Operator
+
+# Reptition
+print("$"*7)
+
+str1 = "HelloWorld"
+print(str1*5)
+print(str1[0:5]*5)
+
+
+# Concatenation
+str1 = 'Hello'
+str2 = 'World'
+str3 = str1 + str2
+print(str3)
+print(str1+" Python")
