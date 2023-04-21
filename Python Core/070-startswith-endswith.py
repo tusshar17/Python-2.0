9c1aba8b01760b700abe446560fdc2aeedc89233
tusshar17 <tr.jakhara019@gmail.com>
2023-04-21 20:23:32 +0530

    startswith endswith

---
+++ Python Core/070-startswith-endswith.py
@@ -0,0 +1,11 @@
+
+str1 = "Hello World"
+str2 = "World Bye"
+
+# startswith()
+print(str1.startswith("Hello"))
+print(str2.startswith("Hello"))
+print()
+# endswith()
+print(str1.endswith("Bye"))
+print(str2.endswith("Bye"))
