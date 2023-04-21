7347fb20662c64832fcf448d67435143a095124a
tusshar17 <tr.jakhara019@gmail.com>
2023-04-21 20:11:33 +0530

    replace() split() join()

---
+++ Python Core/069-replace-split-join.py
@@ -0,0 +1,13 @@
+
+str1 = "Hello World How Are You"
+
+# replace()
+print(str1.replace("World", "Galaxy"))
+
+# split()
+print(str1.split())
+print(str1.split("l"))
+
+# join()
+str2 = ('Hello', 'World', 'How', 'Are', 'You')
+print(" ".join(str2))
