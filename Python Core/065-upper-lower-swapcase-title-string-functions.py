bb4450dd01733077f2640ba7e61256b62a3c5277
tusshar17 <tr.jakhara019@gmail.com>
2023-04-21 17:28:22 +0530

    String Functions

---
+++ Python Core/065-upper-lower-swapcase-title-string-functions.py
@@ -0,0 +1,27 @@
+# String Functions
+
+# upper()
+name = "helloworld"
+print("orignal:", name)
+print("modified:", name.upper())  # it do not change original string
+print()
+
+# lower()
+name = "HELLOWORLD"
+print("orignal:", name)
+print("modified:", name.lower())  # it do not change original string
+print()
+
+
+# swapcase()
+name = "HelloWorld"
+print("orignal:", name)
+print("modified:", name.swapcase())  # it do not change original string
+print()
+
+
+# title()
+name = "hello world how are you"
+print("orignal:", name)
+print("modified:", name.title())  # it do not change original string
+print()
