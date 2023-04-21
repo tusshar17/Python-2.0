9c2fd50df7494aaa84d13068d7b9e09715fb195c
tusshar17 <tr.jakhara019@gmail.com>
2023-04-21 19:40:39 +0530

    islower isupper istitle

--- Python Core/066-isupper-islower-istitle.py
+++ Python Core/066-isupper-islower-istitle.py
@@ -1,21 +1,24 @@
-# isupper()
+
 
 str1 = "HELLO WORLD"
 str2 = "hello world"
 str3 = "Hello World"
 
+# isupper()
 print("isupper()")
 print(str1.isupper())
 print(str2.isupper())
 print(str3.isupper())
 print()
 
+# islower()
 print("islower()")
 print(str1.islower())
 print(str2.islower())
 print(str3.islower())
 print()
 
+# istitle()
 print("istitle()")
 print(str1.istitle())
 print(str2.istitle())
