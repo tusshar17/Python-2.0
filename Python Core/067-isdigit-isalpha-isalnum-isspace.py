e6a094f19d38780c46507722cc5d0248fdeb57b0
tusshar17 <tr.jakhara019@gmail.com>
2023-04-21 19:47:10 +0530

    isdigit() isalpha() isalnum() isspace()

---
+++ Python Core/067-isdigit-isalpha-isalnum-isspace.py
@@ -0,0 +1,32 @@
+
+
+str1 = "HelloWorld"
+str2 = "12345"
+str3 = "hello123"
+
+# isdigit()
+print("isdigit()")
+print(str1.isdigit())
+print(str2.isdigit())
+print(str3.isdigit())
+print()
+
+# isalpha()
+print("isalpha()")
+print(str1.isalpha())
+print(str2.isalpha())
+print(str3.isalpha())
+print()
+
+# isalnum()
+print("isalnum()")
+print(str1.isalnum())
+print(str2.isalnum())
+print(str3.isalnum())
+print()
+
+
+# isspace
+
+str4 = " "
+print(str4.isspace())
