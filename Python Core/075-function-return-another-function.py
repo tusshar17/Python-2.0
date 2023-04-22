4d3cd1a173f8921943da2d6bc292ada53c4387b5
tusshar17 <tr.jakhara019@gmail.com>
2023-04-22 23:58:03 +0530

    function returns another function

---
+++ Python Core/075-function-return-another-function.py
@@ -0,0 +1,16 @@
+# funcion return another function
+
+def fName():
+    return "Tushar"
+
+
+def lName():
+    return "Kumar"
+
+
+def fullName(f, l):
+    return f, l
+
+
+print("My name is", fullName(fName(), lName())
+      [0], fullName(fName(), lName())[1])
