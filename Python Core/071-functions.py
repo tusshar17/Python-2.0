f8935577d35d85e3cb463b87aab50aa2226da751
tusshar17 <tr.jakhara019@gmail.com>
2023-04-22 22:12:35 +0530

    functions

---
+++ Python Core/071-functions.py
@@ -0,0 +1,18 @@
+# Function
+
+# defining a function
+def display():
+    name = "World!"
+    print("Hello", name)
+
+
+# calling a function
+display()
+
+
+# Defining a function with parameter
+def add(num1, num2):
+    print(num1+num2)
+
+
+add(10, 20)
