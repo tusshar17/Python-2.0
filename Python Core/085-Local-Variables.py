48b32b19e46191016a18da9edfab3cd9a81ff796
tusshar17 <tr.jakhara019@gmail.com>
2023-04-27 23:06:39 +0530

    Global & Loacal Variables.py

--- Python Core/085-Local-Variables.py
+++ Python Core/085-Local-Variables.py
@@ -8,3 +8,31 @@ def show():
 show()
 
 # print(name) --> NameError: name 'name' is not defined
+print("\n")
+
+# Global Variables
+
+a = "Global Variable"
+
+
+def show2():
+    x = "Local Variable"
+    print(a, "inside function")
+    print(x, "inside function")
+
+
+show2()
+# print(x, "outside function") --> NameError: name 'x' is not defined
+print(a, "outside function")
+
+print("\n")
+# special case
+i = 1
+
+
+def myfun():
+    # i = i + 1   --> UnboundLocalError: local variable 'i' referenced before assignment
+    print(i)
+
+
+myfun()
