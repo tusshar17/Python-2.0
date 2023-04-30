b6ef6d133d52a472eddfbdb352008ac6ef97d0c3
tusshar17 <tr.jakhara019@gmail.com>
2023-04-30 18:19:59 +0530

    Numpy Built In Maths Function

---
+++ Python Core/092-Numpy-Built-In-Maths-Function.py
@@ -0,0 +1,21 @@
+# Numpy Built In Maths Function
+
+from numpy import *
+
+a = array([25, 81, 49, 64])
+b = array([[4, 9, 16, 25], [36, 16, 4, 49]])
+
+# sum()
+print("Sum:")
+print("A:", sum(a))
+print("B:", sum(b))
+
+# prod()
+print("Prod:")
+print("A:", prod(a))
+print("B:", prod(b))
+
+# sqrt()
+print("Sqrt:")
+print("A:", sqrt(a))
+print("B:", sqrt(b))
