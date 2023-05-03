3a7795d40b8a1f1932353a9e976556e0a22356e9
tusshar17 <tr.jakhara019@gmail.com>
2023-05-03 19:57:20 +0530

    remove() method

---
+++ Python Core/List/007-remove-method.py
@@ -0,0 +1,13 @@
+# remove() method
+
+a = [10, 20, 30, 20, 40, 50]
+
+print("Before Removing:", a)
+
+a.remove(20)
+
+print("After Removing 20:", a)
+
+
+# trying to remove an element which is not in the list
+# a.remove(99) --> ValueError: list.remove(x): x not in list
