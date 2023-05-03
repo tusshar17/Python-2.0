219fa32a8e9a79d6bd3bb1b76d79673f2d3ef0db
tusshar17 <tr.jakhara019@gmail.com>
2023-05-03 20:04:22 +0530

    index() method

---
+++ Python Core/List/008-index-method.py
@@ -0,0 +1,8 @@
+# index() metohd
+
+a = [10, 20, 30, 20, 40, 50]
+
+print(a.index(20))
+
+# trying to find index of an element which is not in the list
+# print(a.index(99))  --> ValueError: 99 is not in list
