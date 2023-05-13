f209194f44efb7512ae56c68f5cd1ba8801abf82
tusshar17 <tr.jakhara019@gmail.com>
2023-05-13 23:30:40 +0530

    Accessing Nested List Using Loop

--- Python Core/List/020-Accessing-Nested-List-using-loop.py
+++ Python Core/List/020-Accessing-Nested-List-using-loop.py
@@ -1,12 +1,29 @@
-# Accessing List Elements using For Loop
+# Accessing Nested List Elements using For Loop
 
-a = [10, 20, 30, 40, 50]
+a = [10, 20, 30, [40, 50, 60]]
 
 # without index
-for x in a:
-    print(x)
 
+# for x in a:
+# for y in x:    --> TypeError: 'int' object is not iterable
+# print(y)
+
+
+for x in a:
+    if type(x) is list:
+        for y in x:
+            print(y)
+
+    else:
+        print(x)
+
+print()
 
 # with index
 for x in range(len(a)):
-    print(x, "index:", a[x])
+    if type(a[x]) is list:
+        for y in range(len(a[x])):
+            print("[{}][{}] :".format(x, y), a[x][y])
+
+    else:
+        print("[{}] :".format(x), a[x])
