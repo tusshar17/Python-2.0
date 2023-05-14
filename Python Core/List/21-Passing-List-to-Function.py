4f46b6a748f0153c70428aa62e2cea52a147a67c
tusshar17 <tr.jakhara019@gmail.com>
2023-05-14 06:56:35 +0530

    Passing/Returning List to/from Function

--- Python Core/List/21-Passing-List-to-Function.py
+++ Python Core/List/21-Passing-List-to-Function.py
@@ -1,6 +1,7 @@
-# Passing List to Function
+# Passing and returning List to/from Function
 
 
+# passing
 def fun(lst):
     print("Type of parameter:", type(lst))
     print(lst)
@@ -8,3 +9,19 @@ def fun(lst):
 
 a = [1, 2, 3, 4, 5]
 fun(a)
+
+print()
+
+# passing and rerturning
+
+
+def double_elem(lst):
+    res = lst * 2
+    res.sort()
+    return res
+
+
+x = [1, 2, 3]
+print("Original List:", x)
+
+print("Doubled Elements:", double_elem(x))
