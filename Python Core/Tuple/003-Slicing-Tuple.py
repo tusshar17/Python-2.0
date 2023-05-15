7913d1b02225eff9b414520793da5a962aed0371
tusshar17 <tr.jakhara019@gmail.com>
2023-05-15 08:17:41 +0530

    Slicing Tuple

---
+++ Python Core/Tuple/003-Slicing-Tuple.py
@@ -0,0 +1,15 @@
+# Slicing Tuple
+
+x = (101, 102, 103, 104, 105, 106, 107, 108, 109, 110)
+
+print("Orignal List:", x)
+
+print("from 1 to 5:", x[1:5])
+
+print("from 5 to last:", x[5:])
+
+print("last 4 elements:", x[-4:])
+
+print("alternate element:", x[::2])
+
+print("reversed :", x[::-1])
