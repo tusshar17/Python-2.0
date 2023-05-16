f1d6f1ca9e6bf711eeb7d551e58bcd8b63ceaa68
tusshar17 <tr.jakhara019@gmail.com>
2023-05-16 23:31:59 +0530

    Accessing Nested Tuple Elements using For Loop

---
+++ Python Core/Tuple/012-Accessing-Nested-Tuple-using-Loop.py
@@ -0,0 +1,29 @@
+# Accessing Nested Tuple Elements using For Loop
+
+a = (10, 20, 30, (40, 50, 60))
+
+# without index
+
+# for x in a:
+# for y in x:    --> TypeError: 'int' object is not iterable
+# print(y)
+
+
+for x in a:
+    if type(x) is tuple:
+        for y in x:
+            print(y)
+
+    else:
+        print(x)
+
+print()
+
+# with index
+for x in range(len(a)):
+    if type(a[x]) is tuple:
+        for y in range(len(a[x])):
+            print("[{}][{}] :".format(x, y), a[x][y])
+
+    else:
+        print("[{}] :".format(x), a[x])
