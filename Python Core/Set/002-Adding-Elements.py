53efb38c68f2f365f9f5affcfc7207ce1067d321
tusshar17 <tr.jakhara019@gmail.com>
2023-05-17 13:12:04 +0530

    Adding Elements in Set

---
+++ Python Core/Set/002-Adding-Elements.py
@@ -0,0 +1,15 @@
+# Adding Elements
+
+a = set()
+
+# adding single element
+a.add("Python")
+print(a)
+
+# adding multiple elements
+a.update([10, 20, 30])
+print(a)
+
+x = "hello"
+a.update(x)
+print(a)
