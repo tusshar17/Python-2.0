20540bb63d7c71423fc0d08e9661b16fc3475789
tusshar17 <tr.jakhara019@gmail.com>
2023-05-20 08:31:53 +0530

    creating dictionary and accessing its elements

--- Python Core/Dictionary/001-Dictionary.py
+++ Python Core/Dictionary/001-Dictionary.py
@@ -1,1 +1,21 @@
 # Dictionary
+
+# creating an empty dictionary
+d = {}
+print(d)
+print(type(d))
+
+students = {101: "raj", 102: "rahul", 103: "sonam"}
+print(students)
+
+print(students[101])
+print(students[102])
+print(students[103])
+
+
+fees = {"raj": 1000, "rahul": 2000, "sonam": 3000, 104: 4000}
+print(fees)
+print(fees["raj"])
+print(fees["rahul"])
+print(fees["sonam"])
+print(fees[104])
