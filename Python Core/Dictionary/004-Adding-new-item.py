d74aff8a17d4b1d6435549e2745828faa33ea9bd
tusshar17 <tr.jakhara019@gmail.com>
2023-05-20 10:21:42 +0530

    Adding new item in the existing dictionary

---
+++ Python Core/Dictionary/004-Adding-new-item.py
@@ -0,0 +1,8 @@
+# Adding new item in the existing dictionary
+
+students = {101: "raj", 102: "rahul", 103: "sonam"}
+print("Original Dictionary:", students)
+print()
+
+students[104] = "simran"
+print("After adding a new item (student):", students)
