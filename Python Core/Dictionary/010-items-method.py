ccd468a768ea60dbab3f8cb0169ab9cc3188aa81
tusshar17 <tr.jakhara019@gmail.com>
2023-05-20 14:19:11 +0530

    items( )

---
+++ Python Core/Dictionary/010-items-method.py
@@ -0,0 +1,15 @@
+# items() method
+
+students = {101: "raj", 102: "rahul", 103: "sonam"}
+
+print("Original Dictionary:", students)
+print()
+
+x = students.items()
+print(x)
+print(type(x))
+print()
+
+lst = list(x)
+print(lst)
+print(type(lst))
