2454806b0df20b8b923b2b40b6cd7fc884e5a83e
tusshar17 <tr.jakhara019@gmail.com>
2023-05-21 13:40:17 +0530

    values( ) method

---
+++ Python Core/Dictionary/012-values-method.py
@@ -0,0 +1,12 @@
+# valuess() method
+
+students = {101: "raj", 102: "rahul", 103: "sonam"}
+
+print("original dictionary:", students)
+
+values_students = students.values()
+print("students.values():", values_students)
+print(type(values_students))
+
+lst = list(values_students)
+print(lst)
