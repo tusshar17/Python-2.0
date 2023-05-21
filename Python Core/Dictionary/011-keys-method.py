a90e0b1d7870a3ae56e5b52ea988e01f828b087f
tusshar17 <tr.jakhara019@gmail.com>
2023-05-21 13:39:46 +0530

    keys( ) method

--- Python Core/Dictionary/011-keys-method.py
+++ Python Core/Dictionary/011-keys-method.py
@@ -6,6 +6,7 @@ print("original dictionary:", students)
 
 keys_students = students.keys()
 print("students.keys():", keys_students)
+print(type(keys_students))
 
 lst = list(keys_students)
 print(lst)
