e6375e1d83c8a384cfbc33edb76c5805053a07b7
tusshar17 <tr.jakhara019@gmail.com>
2023-06-02 14:30:45 +0530

    sorte( ) function

---
+++ Python Core/Some Important Functions/005-sorted-function.py
@@ -0,0 +1,17 @@
+# sorted() function
+
+a = "string"
+b = [1, 3, 2]
+c = (3, 2, 1)
+d = {1, 3, 2}
+e = {2: "a", 1: "b"}
+f = [(2, "a"), (3, "b"), (1, "c")]
+g = [[1, "a"], [3, "b"], [2, "c"]]
+
+print("a", sorted(a))
+print("b", sorted(b))
+print("c", sorted(c))
+print("d", sorted(d))
+print("e", sorted(e))
+print("f", sorted(f))
+print("g", sorted(g))
