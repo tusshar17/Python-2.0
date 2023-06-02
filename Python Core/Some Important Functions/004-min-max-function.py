38f5d0d3ea892a662d6c3442eb5e77dfde06c12b
tusshar17 <tr.jakhara019@gmail.com>
2023-06-02 14:24:22 +0530

    min( ) and max( ) function

---
+++ Python Core/Some Important Functions/004-min-max-function.py
@@ -0,0 +1,46 @@
+# min() and max() functions
+
+# min()
+print("====min()====")
+a = "string"
+b = [1, 2, 3]
+c = (1, 2, 3)
+d = {1, 2, 3}
+e = {1: "a", 2: "b"}
+f = [(1, "a"), (2, "b"), (3, "c")]
+g = [[1, "a"], [2, "b"], [3, "c"]]
+
+print("a:", min(a))
+print("b:", min(b))
+print("c:", min(c))
+print("d:", min(d))
+print("e:", min(e))
+print("f:", min(f))
+print("g:", min(g))
+
+
+print(min(5, 3, 54, 7))
+
+print()
+
+
+# max()
+print("====max()====")
+a = "string"
+b = [1, 2, 3]
+c = (1, 2, 3)
+d = {1, 2, 3}
+e = {1: "a", 2: "b"}
+f = [(1, "a"), (2, "b"), (3, "c")]
+g = [[1, "a"], [2, "b"], [3, "c"]]
+
+print("a:", max(a))
+print("b:", max(b))
+print("c:", max(c))
+print("d:", max(d))
+print("e:", max(e))
+print("f:", max(f))
+print("g:", max(g))
+
+
+print(max(5, 3, 54, 7))
