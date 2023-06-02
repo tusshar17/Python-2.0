4028b508c684bc05ee4b4890d20fae97844a45e0
tusshar17 <tr.jakhara019@gmail.com>
2023-06-02 11:36:05 +0530

    len( ) function

---
+++ Python Core/Some Important Functions/003-len-function.py
@@ -0,0 +1,17 @@
+# len() function
+
+a = "String"
+b = [1, 2, 3]
+c = (1, 2, 3)
+d = {1, 2, 3}
+e = {1: "a", 2: "b"}
+f = [(1, "a"), (2, "b"), (3, "c")]
+g = [[1, "a"], [2, "b"], [3, "c"]]
+
+print("a", len(a))
+print("b", len(b))
+print("c", len(c))
+print("d", len(d))
+print("e", len(e))
+print("f", len(f))
+print("g", len(g))
