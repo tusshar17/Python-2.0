670f2dca7a62be1dcf20a63a6c171eb697eedb66
tusshar17 <tr.jakhara019@gmail.com>
2023-06-09 23:49:11 +0530

    Nested Class

--- Advance Python/OOP/011-Nested-Class.py
+++ Advance Python/OOP/011-Nested-Class.py
@@ -1,1 +1,27 @@
 # Nested Class
+
+
+class Team:  # outer class
+    def __init__(self) -> None:
+        self.name = "Joy"
+        self.device = self.Device()  # creating inner class object
+
+    def show(self):
+        print("Name:", self.name)
+
+    class Device:  # inner class
+        def __init__(self) -> None:
+            self.name = "Laptop"
+            self.price = 500000
+
+        def display(self):
+            print("Devic Name:", self.name)
+            print("Devic Price:", self.price)
+
+
+a = Team()
+# print(a.name)
+a.show()
+
+# print(a.device.name)
+a.device.display()
