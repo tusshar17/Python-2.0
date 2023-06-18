2e6560bb5e651f59cd40daa50395b6d31c273ef2
tusshar17 <tr.jakhara019@gmail.com>
2023-06-19 02:56:12 +0530

    Multilevel Inheritance

--- Advance Python/OOP/016-Multilvel-inheritance.py
+++ Advance Python/OOP/016-Multilvel-inheritance.py
@@ -1,1 +1,32 @@
 # Multilevel Inheritance
+
+class Father:
+    def __init__(self) -> None:
+        print("Father Class Constructor")
+
+    def showF(self):
+        print("Father Class Method")
+
+
+class Son(Father):
+    def __init__(self) -> None:
+        super().__init__()
+        print("Son Class Constructor")
+
+    def showS(self):
+        print("Son Class Method")
+
+
+class GrandsSon(Son):
+    def __init__(self) -> None:
+        super().__init__()
+        print("Grand Son Class Constructor")
+
+    def showG(self):
+        print("Grand Son Class Method")
+
+
+g = GrandsSon()
+g.showG()
+g.showS()
+g.showF()
