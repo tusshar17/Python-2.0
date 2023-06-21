8933cb63fbf6d88f42ec04d2c4c687c97dfb9e01
tusshar17 <tr.jakhara019@gmail.com>
2023-06-21 09:59:57 +0530

    Multiple Inheritance

--- Advance Python/OOP/018-Multiple-Inheritance.py
+++ Advance Python/OOP/018-Multiple-Inheritance.py
@@ -1,1 +1,21 @@
 # Multiple Inheritance
+
+class Father:
+    def showF(self):
+        print("Father Class Method")
+
+
+class Mother:
+    def showM(self):
+        print("Mother Class Method")
+
+
+class Son(Father, Mother):
+    def showS(self):
+        print("Son Class Method")
+
+
+s = Son()
+s.showS()
+s.showF()
+s.showM()
