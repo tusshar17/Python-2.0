2729a7dffb6af7eb74ede8015fd4dff3257debe4
tusshar17 <tr.jakhara019@gmail.com>
2023-06-23 08:33:50 +0530

    Multiple Inheritance

--- Advance Python/OOP/018-Multiple-Inheritance.py
+++ Advance Python/OOP/018-Multiple-Inheritance.py
@@ -1,16 +1,29 @@
 # Multiple Inheritance
+# MRO - Method Resolution Order
 
 class Father:
+    def __init__(self) -> None:
+        super().__init__()
+        print("Father Class Constructor")
+
     def showF(self):
         print("Father Class Method")
 
 
 class Mother:
+    def __init__(self) -> None:
+        super().__init__()
+        print("Mother Class Constructor")
+
     def showM(self):
         print("Mother Class Method")
 
 
 class Son(Father, Mother):
+    def __init__(self) -> None:
+        super().__init__()
+        print("Son Class Constructor")
+
     def showS(self):
         print("Son Class Method")
 
