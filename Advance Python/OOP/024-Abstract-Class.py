37185bdd5cac31276471c1760a2990d60d168a41
tusshar17 <tr.jakhara019@gmail.com>
2023-07-04 01:20:09 +0530

    Abstract Class

--- Advance Python/OOP/024-Abstract-Class.py
+++ Advance Python/OOP/024-Abstract-Class.py
@@ -1,1 +1,24 @@
 # Abstract Class
+
+from abc import ABC, abstractmethod
+
+class Father(ABC):
+    @abstractmethod
+    def disp(self):
+        pass
+
+    def show(self):
+        print("Concrete Method")
+
+
+class Son(Father):
+    def disp(self):
+        print("Child Class")
+        print("Defining Abstract Method")
+
+
+# c = Father() -->TypeError: Can't instantiate abstract class Father with abstract methods disp
+
+c = Son()
+c.disp()
+c.show()
